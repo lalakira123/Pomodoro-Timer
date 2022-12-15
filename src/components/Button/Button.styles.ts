452f3a4690a7ae05d99e2e188c9c9523c83b4d45
Laerte <laerteakira@gmail.com>
2022-12-15 11:14:15 -0300

    Setup Colors and Types

--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -17,7 +17,8 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 50px;
 
-  ${props => {
+  background-color: ${props => props.theme["green-500"]};
+  /* ${props => {
     return css`background-color: ${buttonVariants[props.variant]}`
-  }}
+  }} */
 `
