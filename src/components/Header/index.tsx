b29f55487e01dadd75852a8c68c63215de21126d
Laerte <laerteakira@gmail.com>
2022-12-15 16:08:16 -0300

    feat: header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import { NavLink } from "react-router-dom"
+import { Timer, Scroll } from "phosphor-react"
+
+import { HeaderContainer } from "./styles"
+
+import logoIgnite from '../../assets/logoIgnite.svg'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={logoIgnite} alt="" />
+      <nav>
+        <NavLink to="/">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
